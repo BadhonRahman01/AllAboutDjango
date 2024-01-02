80770902a3d43f1723f5317358a2da1ed09f3aa0
BadhonRahman01 <badhon.rahman184@gmail.com>
2024-01-02 23:06:16 +0200

    2.14Done

--- storefront2/store/serializers.py
+++ storefront2/store/serializers.py
@@ -1,18 +1,38 @@
 from rest_framework import serializers
 from decimal import Decimal
-from .models import Product
+from .models import Product, Collection
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
+
+
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id', 'title']
+    # id = serializers.IntegerField()
+    # title = serializers.CharField(max_length=255)
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'unit_price','price_with_tax','collection']
+    # id = serializers.IntegerField()
+    # title = serializers.CharField(max_length=255)
+    # price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
 
+    # collection = serializers.HyperlinkedRelatedField(
+    #     queryset=Collection.objects.all(),
+    #     view_name='collection-detail',
+    #     lookup_field='pk'
+    # )
 
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
 
-
+    # def validate(self, data):
+    #     if data['title'] == data['description']:
+    #         raise serializers.ValidationError('Title and description must be different from one another!')
+    #     return data
 
 #    name = serializers.CharField(max_length=100)
     # price = serializers.DecimalField(max_digits=10, decimal_places=2)
