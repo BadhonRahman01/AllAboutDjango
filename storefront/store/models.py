52d3047925efd0d3535169ec36be40af959a7861
BadhonRahman01 <badhon.rahman184@gmail.com>
2024-07-01 23:43:45 +0300

    Admin03Done

--- storefront/store/models.py
+++ storefront/store/models.py
@@ -12,6 +12,9 @@ class Collection(models.Model):
     featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+')
     def __str__(self) -> str:
         return self.title
+    
+    class Meta:
+        ordering = ['title']
 
 class Product(models.Model):
     # sku = models.CharField(max_length=10, primary_key=True)
@@ -23,7 +26,11 @@ class Product(models.Model):
     last_updated = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
     promotions = models.ManyToManyField(Promotion)
-
+    def __str__(self) -> str:
+        return self.title
+    
+    class Meta:
+        ordering = ['title']
 
 
 class Customer(models.Model):
--- storefront2/store/urls.py
+++ storefront2/store/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 # URLConf
 urlpatterns = [
-    path('products/', views.PrtoductList.as_view()),
+    path('products/', views.ProductList.as_view()),
     path('products/<int:pk>', views.PrtoductDetail.as_view()),
     path('collections/<int:pk>', views.CollectionDetail.as_view(), name='collection-detail'),
     path('collections/', views.CollectionList.as_view()),
